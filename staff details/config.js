6396da44df14c33eb5dd8c1c0b16bd421c50b6e1
saisoukhya14-cmyk <saisoukhya14@gmail.com>
2025-12-10 14:46:42 +0530

    Update config.js

--- staff details/config.js
+++ staff details/config.js
@@ -2,7 +2,7 @@
 // Configuration for Supabase and OpenAI
 window.APP_CONFIG = {
     SUPABASE_URL: 'https://cwaayduboikqihempgbn.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3YWF5ZHVib2lrcWloZW1wZ2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNzI1MDMsImV4cCI6MjA4MDc0ODUwM30.mZULa8vhq_o4a6J631he9PCJy_iTwm2vhMWFtJ4AoCE',
+    SUPABASE_KEY: 'sb_publishable_Y707XoorB4o2h5sCGaO8wA_4EeT4Rz-',
     // Add your Gemini API key here (Get it from https://aistudio.google.com/app/apikey)
     GEMINI_API_KEY: 'AIzaSyAUT2oi5LtVgtdfLFaJwadaU2QY-o1gKhE'
 };
